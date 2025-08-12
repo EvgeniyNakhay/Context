7c3b304bc86b80da4bc2c8d674a89fbb27cfe4b9
EvgeniyNakhay <qwert57894@gmail.com>
2025-08-12 20:50:40 +0300

    added Toggler

--- src/components/Root/index.jsx
+++ src/components/Root/index.jsx
@@ -1,3 +1,18 @@
-const Root = () => <div>There will be Dark Theme</div>;
+import { ThemeContext, themes } from "../../contexts/ThemeContext";
+import Toggle from "../Toggle";
+
+const Root = () => (
+  <ThemeContext.Consumer>
+    {({ theme, setTheme }) => (
+      <Toggle
+        onChange={() => {
+          if (theme === themes.light) setTheme(theme.dark);
+          if (theme === themes.dark) setTheme(theme.light);
+        }}
+        value={theme === themes.dark}
+      />
+    )}
+  </ThemeContext.Consumer>
+);
 
 export default Root;
