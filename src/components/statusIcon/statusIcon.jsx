218538159cef449a40d4f93d1dc6a37e960ff96e
EvgeniyNakhay <qwert57894@gmail.com>
2025-08-17 23:52:32 +0300

    centered components

--- src/components/statusIcon/statusIcon.jsx
+++ src/components/statusIcon/statusIcon.jsx
@@ -1,7 +1,12 @@
 import styles from "./index.module.scss";
 
 const StatusIcon = () => (
-  <img src="./sun.png" alt="sun_logo" className={styles.root} />
+  <img
+    src="
+src/components/statusIcon/sun.png"
+    alt="sun_logo"
+    className={styles.root}
+  />
 );
 
 export default StatusIcon;
