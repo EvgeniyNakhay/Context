592af5d09eaed2b881fb70938bb6657ccc3c1c65
EvgeniyNakhay <qwert57894@gmail.com>
2025-08-18 21:54:33 +0300

    centered elements

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.scss";
+// import "./index.scss";
+import styles from "./components/Toggle/index.module.scss";
 import Root from "./components/Root";
 import ThemeProvider from "./providers/ThemeProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
-      <Root />
+      <Root className={styles.root} />
     </ThemeProvider>
   </StrictMode>
 );
